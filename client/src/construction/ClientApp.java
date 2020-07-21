fd797e522f09f0d79fb41f4ae8a273719645cfee
P.PHANINDRANATH <P.PHANINDRANATH@192.168.0.6>
2020-07-21 21:40:07 +0530

    client

---
+++ client/src/construction/ClientApp.java
@@ -0,0 +1,12 @@
+package construction;
+
+public class ClientApp {
+	public static void main(String[] args)
+	{
+		Construction construction = new Construction();
+		construction.takeInput();
+		construction.calculateAndDisplayTotalCost();
+		
+	}
+
+}
